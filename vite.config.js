6bafb4fe87b27ba48461c9e34c25ce0f4a391557
WorthEx <7hecakeis4.l1e@gmail.com>
2024-10-07 03:50:30 +0500

    попытка поставить нужный порт #2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	port: 8080,
 	plugins: [
 		vue(),
 	],
@@ -13,5 +12,9 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
+	},
+	server: {
+		host: '0.0.0.0',
+		port: process.env.PORT || 5173
 	}
 })
